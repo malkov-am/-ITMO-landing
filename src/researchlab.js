0562654e4c923f3d8285e858f45d18097637a050
Andrei Malkov <thewildcat@yandex.ru>
2022-04-03 18:13:20 +0300

    add list-menu

--- src/researchlab.js
+++ src/researchlab.js
@@ -1,1 +1,11 @@
 // TO DO
+//Открытие меню
+const burgerBtn = document.querySelector('.menu-bar__burger-btn');
+const menu = document.querySelector('.menu-bar__burgernav');
+
+burgerBtn.addEventListener('click', openMenu);
+
+function openMenu() {
+  burgerBtn.classList.toggle('menu-bar__burger-btn_opened');
+  menu.classList.toggle('menu-bar__burgernav_opened');
+}
